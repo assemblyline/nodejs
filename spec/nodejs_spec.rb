9fd7b2bd20bbb830900c7f5baf6a90be0c381601
Ed Robinson <ed@reevoo.com>
2016-05-11 10:11:39 +0100

    Bump versions

--- spec/nodejs_spec.rb
+++ spec/nodejs_spec.rb
@@ -5,22 +5,17 @@ describe 'Node.js' do
     expect(node_version).to eq '6.1.0'
   end
 
-  it 'installs version 5.10.1', :'5.10.1' do
-    expect(node_version).to eq '5.10.1'
+  it 'installs version 5.11.1', :'5.11.1' do
+    expect(node_version).to eq '5.11.1'
   end
 
-  it 'installs version 4.4.2', :'4.4.2' do
-    expect(node_version).to eq '4.4.2'
+  it 'installs version 4.4.4', :'4.4.4' do
+    expect(node_version).to eq '4.4.4'
   end
 
-  it 'installs version 0.12.13', :'0.12.13' do
-    expect(node_version).to eq '0.12.13'
+  it 'installs version 0.12.14', :'0.12.14' do
+    expect(node_version).to eq '0.12.14'
   end
-
-  it 'installs version 0.10.44', :'0.10.44' do
-    expect(node_version).to eq '0.10.44'
-  end
-
 
   it 'installs npm version 3.8.9' do
     expect(npm_version).to eq '3.8.9'
